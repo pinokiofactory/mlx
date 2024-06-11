eab84dfe4adabd602facfe22547d10fd2b642b68
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-11 14:19:39 -0400

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/qnguyen3/chat-with-mlx app",
+          "git clone https://github.com/cocktailpeanut/chat-with-mlx app",
         ]
       }
     },
@@ -28,18 +28,16 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio devicetorch",
-          "pip install -r requirements.txt"
+          "pip install -e ."
         ]
       }
     },
-    //  Uncomment this step to add automatic venv deduplication (Experimental)
-    //  {
-    //    method: "fs.link",
-    //    params: {
-    //      venv: "app/env"
-    //    }
-    //  },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    },
     {
       method: "notify",
       params: {
--- start.js
+++ start.js
@@ -8,7 +8,7 @@ module.exports = {
         env: { },                   // Edit this to customize environment variables (see documentation)
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "python app.py",    // Edit with your custom commands
+          "chat-with-mlx"
         ],
         on: [{
           // The regular expression pattern to monitor.
@@ -31,13 +31,12 @@ module.exports = {
         url: "{{input.event[0]}}"
       }
     },
-//    Uncomment this step to enable local wifi sharing (access the app from devices on the same network)
-//    {
-//      method: "proxy.start",
-//      params: {
-//        uri: "{{local.url}}",
-//        name: "Local Sharing"
-//      }
-//    }
+    {
+      method: "proxy.start",
+      params: {
+        uri: "{{local.url}}",
+        name: "Local Sharing"
+      }
+    }
   ]
 }
