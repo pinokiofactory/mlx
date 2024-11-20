eedc5a1a3f57fbd7d78ad36d3f934b20372fac4c
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-20 15:34:33 -0500

    update

--- install.js
+++ install.js
@@ -26,7 +26,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install -U huggingface_hub==0.25.2"
+          "pip install -U huggingface-hub==0.24.7"
         ]
       }
     },
